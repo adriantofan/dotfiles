4a1665653571f464b586c4ab9ad3ab26f83ccf80
Adrian Tofan <adrian@tofan.co>
2015-02-20 09:10:46 +0100

    aded xcode + files

--- brew.sh
+++ brew.sh
@@ -99,4 +99,15 @@ brew install git
 # brew install node
 
 # Remove outdated versions from the cellar.
+
+brew install caskroom/cask/brew-cask
+
+
+brew cask install iterm2
+brew cask install 1password
+brew cask install skype
+brew tap caskroom/fonts
+brew cask install font-source-code-pro
+
+
 brew cleanup
